a4676a671f0f05d67c5df4a7180a8ed0f038568b
Brandy Quinlan <brandyquinlan.bootcamp@gmail.com>
2021-02-16 11:01:02 -0600

    initial commit of js files

--- index.js
+++ index.js
@@ -1,66 +1,77 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const Manager = require('.lib/Manager');
 
-const generateHTML = (answers) =>
-    `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+// Initialize a new Game object
+const manager = new Manager();
 
-inquirer
-    .prompt([{
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?',
-        },
-        {
-            type: 'input',
-            name: 'location',
-            message: 'Where are you from?',
-        },
-        {
-            type: 'input',
-            name: 'hobby',
-            message: 'What is your favorite hobby?',
-        },
-        {
-            type: 'input',
-            name: 'food',
-            message: 'What is your favorite food?',
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub Username',
-        },
-        {
-            type: 'input',
-            name: 'linkedin',
-            message: 'Enter your LinkedIn URL.',
-        },
-    ])
-    .then((answers) => {
-        const htmlPageContent = generateHTML(answers);
+// Start playing
+manager.play();
 
-        fs.writeFile('index.html', htmlPageContent, (err) =>
-            err ? console.log(err) : console.log('Successfully created index.html!')
-        );
-    });
+
+
+
+// const fs = require('fs');
+// const {type} = require('.lib/Manager')
+
+
+// const generateHTML = (answers) =>
+//     `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+//   <title>Document</title>
+// </head>
+// <body>
+//   <div class="jumbotron jumbotron-fluid">
+//   <div class="container">
+//     <h1 class="display-4">${answers.name}</h1>
+//     <p class="lead">${answers.type}</p>
+//     <ul class="list-group">
+//       <li class="list-group-item">id: ${answers.id}</li>
+//       <li class="list-group-item">email: ${answers.email}</li>
+//       <li class="list-group-item">office: ${answers.office}</li>
+//     </ul>
+//   </div>
+// </div>
+// </body>
+// </html>`;
+
+// inquirer
+//     .prompt([{
+//             type: 'input',
+//             name: 'name',
+//             message: 'What is the team member\'s name?'
+//         },
+//         {
+//             type: 'input',
+//             name: 'id',
+//             message: 'What is the team member\'s id?'
+//         },
+//         {
+//             type: 'input',
+//             name: 'email',
+//             message: 'What is the team member\'s email?'
+//         },
+//         {
+//             type: 'input',
+//             name: 'office',
+//             message: 'What is the team member\'s office number?'
+//         },
+//         {
+//             type: 'list',
+//             name: 'type',
+//             message: 'What type of tem member would you like to add?',
+//             choices: ['Engineer',
+//                 'Intern',
+//                 'I do not want to add another member'
+//             ]
+//         }
+//     ])
+//     .then((answers) => {
+//         const htmlPageContent = generateHTML(answers);
+
+//         fs.writeFile('./output/index.html', htmlPageContent, (err) =>
+//             err ? console.log(err) : console.log('Successfully created index.html!')
+//         );
+//     });
